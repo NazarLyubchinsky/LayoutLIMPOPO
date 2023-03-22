7522a6ffbcd4170f88a855c1ab458729f22cef04
NazarLyubchinsky <nazar.lyubchinskyi@gmail.com>
2023-03-22 11:36:03 +0200

    LIMPOPO

--- src/index.js
+++ src/index.js
@@ -9,116 +9,126 @@ import { Elements } from "./scripts/modules/elements";
 // Import methods
 import { includeHTML } from "./scripts/methods/_includeComponents";
 
-// Bisiness logic
-document.addEventListener('DOMContentLoaded', () => {
-	includeHTML();
-	console.log(calcSubtractNumbers(10, 5));
-	console.log(calcSumNumbers(10, 5));
-
-});
-
-function showData(data, selector) {
-	const element = document.querySelector(selector);
-	element.innerHTML = data;
-}
-
-function transformData(data) {
-	const { name, surname, patronic } = data;
-	const surnameTranform = surname[0].toUpperCase() + surname.slice(1);
-	return `${surnameTranform} ${name[0]}. ${patronic[0]}.`;
-}
-
-function getDataForm() {
-	const NAME = document.querySelector('#name').value;
-	const SURNAME = document.querySelector('#surname').value;
-	const PATRONIC = document.querySelector('#patronymic').value;
-
-	const DATA = {
-		name: NAME,
-		surname: SURNAME,
-		patronic: PATRONIC
-	}
-	return transformData(DATA);
-}
-
-export function initialsBtn() {
-	const BTN_FORM = document.querySelector('#btn');
-	BTN_FORM.addEventListener('click', (e) => {
-		e.preventDefault();
-		let initials = getDataForm();
-		showData(initials, '.output-info');
-	});
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-	initialsBtn();
-});
-
-
-// SLIDER //
-
-const PARAMS = {
-
-
-	// Optional parameters
-
-	loop: true,
-	slidesPerView: 4,
-	slidesPerGroup: 1,
-	spaceBetween: 30,
+// SWIPER	PATRIONS
+var swiper = new Swiper(".mySwiper", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	
+	slidesPerView: 1.6,
 	breakpoints: {
-		// when window width is >= 320px
 		320: {
 			slidesPerView: 1,
-			spaceBetween: 20
+			spaceBetween: 50,
 		},
-		// when window width is >= 480px
-		480: {
-			slidesPerView: 2,
+
+
+		768: {
+			slidesPerView: 1.7,
 			spaceBetween: 30
 		},
-		// when window width is >= 640px
+
 		992: {
-			slidesPerView: 4,
+			slidesPerView: 1.6,
 			spaceBetween: 40
 		}
 
 	},
-	// If we need pagination
-	pagination: {
-		el: '.swiper-pagination',
-	},
 
-	// Navigation arrows
+});
+
+
+
+// SWIPER PATRIONS END
+
+// SWIPER ANIMALS 
+
+var swiper2 = new Swiper(".swiperAnimals", {
 	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
 	},
+	slidesPerView: 6,
+	spaceBetween: 30,
+	watchSlidesProgress: true,
 
-	// And if we need scrollbar
-	scrollbar: {
-		el: '.swiper-scrollbar',
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 100,
+		},
+
+
+		768: {
+			slidesPerView: 3.2,
+			spaceBetween: 10,
+
+		},
+
+		992: {
+			slidesPerView: 5.3,
+			spaceBetween: 30,
+		}
+
 	},
-}
-const swiper = new Swiper('.swiper', PARAMS);
+});
 
 
+// TABS animals
+
+const TABS = document.querySelectorAll('.tabs__item');
+const CONTENT = document.querySelectorAll('.tabs__content');
 
 
+TABS.forEach((tab) => {
+	tab.addEventListener('click', () => {
+		TABS.forEach((tab) => {
+			tab.classList.remove('selected');
+		});
+		tab.classList.add('selected');
+		const DATA_ATTRIBUTE = tab.getAttribute('data-products');
+		CONTENT.forEach((item) => {
+			item.classList.add('d-none');
+			if (item.getAttribute('data-products') == DATA_ATTRIBUTE) {
+				item.classList.remove('d-none');
+			}
+		});
+	})
+});
 
 
+// TABS END
 
-// var swiper = new Swiper(".myswiper", {
-// 	pagination: {
-// 		el: ".swiper-pagination",
-// 		type: "fraction",
-// 	},
-// 	navigation: {
-// 		nextEl: ".swiper-button-next",
-// 		prevEl: ".swiper-button-prev",
-// 	},
-// });
+// news swiper
 
+var swiper3 = new Swiper(".newsSwiper", {
+	navigation: {
+		nextEl: ".swiper-button-next",
+		prevEl: ".swiper-button-prev",
+	},
+	slidesPerView: 5,
+	spaceBetween: 30,
+	breakpoints: {
+		320: {
+			slidesPerView: 1,
+			spaceBetween: 10,
+		},
+
+
+		768: {
+			slidesPerView: 3.3,
+			spaceBetween: 30,
+
+		},
+
+		992: {
+			slidesPerView: 4.4,
+			spaceBetween: 30,
+		}
+
+	},
+});
 
 
 
